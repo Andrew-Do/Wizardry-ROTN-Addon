7e9e3493d04a63a0096fcab0dacbd476f8664f6f
Andrew-Do <49578245+Andrew-Do@users.noreply.github.com>
2022-11-04 12:08:59 -0700

    Cleanup

--- src/main/java/com/a/wizardry2/mixin/wizardry/mItemSpellBookClient.java
+++
@@ -1,8 +0,0 @@
-package com.a.wizardry2.mixin.wizardry;
-
-import electroblob.wizardry.item.ItemSpellBook;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(ItemSpellBook.class)
-public class mItemSpellBookClient {
-}
